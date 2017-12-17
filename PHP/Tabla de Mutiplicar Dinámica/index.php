e5845f8d2c9e168cb59a75cf01fb958d78c0eff1
Angel Erazo <mensajero27@gmail.com>
2017-12-16 20:24:16 -0600

    listo

--- PHP/Tabla de Mutiplicar Dinámica/index.php
+++ PHP/Tabla de Mutiplicar Dinámica/index.php
@@ -4,36 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tabla de Multiplicar Dinámica</title>
+    <title>Tabla de Multiplicar PHP</title>
 </head>
 <body>
-
-<form action="#" method="post">
-<input type="text" name="num1" >
-<input type="text" name="num2" >
-<input type="submit" value="Calcular">
-
-
-</form>
-
-<?php
-
-$num1=$_POST['num1'];
-$num2=$_POST['num2'];
-$resultado;
-$i;
-
-for ($num1=$num1;$num1<=$num2;$num1++){
-    for($i=1;$i<=10;$i++){
-        $resultado=$num1 * $i;
-        echo("<br>");
-        echo("$num1 x $i = $resultado");
-    }
-}
+    <form action="#" method="post">
+        <input type="text" name="n1" >
+        <input type="text" name="n2">
+        <input type="submit" value="Calcular">
+    </form>
 
 
+    <?php
 
-?>
-    
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+        $num1=$_POST['n1'];
+        $num2=$_POST['n2'];
+        $resultado;
+        $i;
+    if($num1!="" || $num2!=""){
+     for($num1=$num1;$num1<=$num2;$num1++){
+            for($i=1;$i<=10;$i++){
+            $resultado=$num1*$i;
+            echo("<br>");
+            echo("$num1 x $i = $resultado");
+            }
+        }
+
+     }
+    ?>
+
 </body>
 </html>
